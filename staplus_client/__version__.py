db43844dd7ea46c38fc1f93af9e2358fd2c3a9cf
Andreas Matheus <andreas.matheus@secure-dimensions.de>
2024-05-22 09:59:51 +0200

    keep unittest happy

--- staplus_client/__version__.py
+++ staplus_client/__version__.py
@@ -1,8 +1,1 @@
-__title__ = 'STAplus Python Client'
-__version__ = '0.8'
-__license__ = 'LGPL3'
-__author__ = 'Andreas Matheus'
-__copyright__ = 'Secure Dimensions'
-__contact__ = 'am@secure-dimensions.de'
-__url__ = 'https://github.com/securedimensions/STAplus-Python-Client'
-__description__ = 'A client library to facilitate interaction with a STAplus SensorThings API Server'
+../__version__.py
